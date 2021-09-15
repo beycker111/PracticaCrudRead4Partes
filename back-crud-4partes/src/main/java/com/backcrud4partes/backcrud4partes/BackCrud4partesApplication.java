b2bd7b9aee5c1460a40cf5c17a841c9569df254b
Beycker <beycker_111@hotmail.com>
2021-09-15 08:15:35 -0500

    Creacion de los dos proyectos - Front y Back

---
+++ back-crud-4partes/src/main/java/com/backcrud4partes/backcrud4partes/BackCrud4partesApplication.java
@@ -0,0 +1,13 @@
+package com.backcrud4partes.backcrud4partes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCrud4partesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCrud4partesApplication.class, args);
+	}
+
+}
---
+++ back-crud-4partes/src/test/java/com/backcrud4partes/backcrud4partes/BackCrud4partesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backcrud4partes.backcrud4partes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCrud4partesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
